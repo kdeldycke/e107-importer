kdeldycke/e107-importer

commit e47053a92983393a97941e4cca8b7849d41d1163

Use bbPress functions to get post types.

e107-redirector.php
@@ -115,7 +115,7 @@ class e107_Redirector {
               } elseif ($ctype == 'forum') {
                 $link = bbp_get_forum_permalink($content_id);
               } elseif ($ctype == 'forum_post') {
-                if (get_post($content_id)->post_type == 'bbp_topic')
+                if (get_post($content_id)->post_type == bbp_get_topic_post_type())
                   $link = bbp_get_topic_permalink($content_id);
                 else
                   $link = bbp_get_reply_permalink($content_id);
@@ -145,6 +145,8 @@ class e107_Redirector {
             $feed_content = 'comments_';
             break;
           case 'forum':
+            # TODO: get forum feed
+            # XXX Not implemented yet in bbPress, see: http://trac.bbpress.org/ticket/1422
             break;
           case '6':
           case 'threads':      # TODO: Test both
@@ -158,8 +160,8 @@ class e107_Redirector {
           case '11':
           case 'name':
           case 'forumname':
-            #$feed_content = 'forum_';
             # TODO: get the feed of the thread (= feed of the topic + replies) to which the post is part of (all e107 feeds here returns to forum/forum_viewtopic.php?XXX like URLs anyway).
+            # XXX Not implemented yet in bbPress, see: http://trac.bbpress.org/ticket/1422
             break;
         }
       if (sizeof($feed_params) > 1)
@@ -183,9 +185,11 @@ class e107_Redirector {
             $feed_type = 'atom';
             break;
         }
+      if ($feed_content != '') {
       // Redirect to proper WordPress feed
-      $wordpress_feed = $feed_content.$feed_type.'_url';
-      wp_redirect(get_bloginfo($wordpress_feed), $status = 301);
+        $wordpress_feed = $feed_content.$feed_type.'_url';
+        wp_redirect(get_bloginfo($wordpress_feed), $status = 301);
+      }
     }
 
     // Generic redirects and catch-alls
@@ -196,8 +200,8 @@ class e107_Redirector {
         wp_redirect(get_option('siteurl'), $status = 301);
 
       // Redirect to bbPress home page
-#      elseif (preg_match('/^.*\/forum(.*).php.*$/i', $requested))
-#        wp_redirect(XXX, $status = 301);
+      elseif (preg_match('/^.*\/forum(.*).php.*$/i', $requested))
+        wp_redirect(get_option('siteurl').'/'.get_option('_bbp_root_slug'), $status = 301);
 
       // Redirects to forum stats
 #      elseif (preg_match('/^.*\/forum_stats\.php.*$/i', $requested))

e107-importer.php
@@ -714,7 +714,7 @@ class e107_Import extends WP_Importer {
         , 'post_date_gmt'  => $this->mysql_date($forum_datestamp)  //XXX ask or get the time offset ?
         , 'post_content'   => $forum_description
         , 'post_title'     => $forum_name
-        , 'post_type'      => 'bbp_forum'
+        , 'post_type'      => bbp_get_forum_post_type()
         , 'comment_status' => 'closed'
         , 'ping_status'    => 'closed'
         , 'post_parent'    => $updated_parent
@@ -794,10 +794,10 @@ class e107_Import extends WP_Importer {
       // Top message of threads are topics, attached to a forum.
       // Others are replies, attached to a topic.
       if ($thread_parent > 0) {
-        $post_type = 'bbp_reply';
+        $post_type = bbp_get_reply_post_type();
         $thread_parent_id = (int) $this->forum_post_mapping[$thread_parent];
       } else {
-        $post_type = 'bbp_topic';
+        $post_type = bbp_get_topic_post_type();
         $thread_parent_id = $this->forum_mapping[$thread_forum_id];
       }
 

commit b0cf532e07e771f955ebd3d5d4c926965ba30e2a

Fix news feed redirections bug introduced in e47053a92983393a97941e4cca8b7849d41d1163.

e107-redirector.php
@@ -229,7 +229,7 @@ class e107_Redirector {
     // Redirect feeds as explained there: http://kevin.deldycke.com/2007/05/feedburner-and-e107-integration/
     if (preg_match('/\/e107_plugins\/(?:rss_menu\/|forum\/e_)rss\.php(?:%3F|\?)?(.*)/i', $url, $matches)) {
       // Default feed redirections
-      $feed_content = '';
+      $feed_content = null;
       $feed_type    = 'rss2';
       // Analyze feed parameters
       $feed_params = explode('.', $matches[1]);
@@ -284,7 +284,7 @@ class e107_Redirector {
             $feed_type = 'atom';
             break;
         }
-      if ($feed_content != '') {
+      if (!is_null($feed_content)) {
       // Redirect to proper WordPress feed
         $wordpress_feed = $feed_content.$feed_type.'_url';
         return get_bloginfo($wordpress_feed);